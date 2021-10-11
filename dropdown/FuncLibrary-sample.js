1b4adfd0ad360efd8b9144b28f08531ad164952f
Alireza Ataei <alireza.ataee01@gmail.com>
2021-10-11 17:05:55 -0400

    update

---
+++ dropdown/FuncLibrary-sample.js
@@ -0,0 +1,272 @@
+// var dropdownSubMenuDiv = [];
+window.addEventListener("load", ()=>{
+
+  // dropdown handler
+  const dropdowns = document.querySelectorAll("ax-elements .dropdown");
+  const dropdownHeaders = document.querySelectorAll("ax-elements .dropdown .dropdownHead");
+  const dropdownSubMenuDiv = document.querySelectorAll("ax-elements .dropdown .dropdownBody .menu li[subtrigger='click'] > div");
+  const dropdownSubMenu = document.querySelectorAll("ax-elements .dropdown .dropdownBody .menu li[subtrigger='click']");
+
+  // handle submenu clicks
+  dropdownSubMenuDiv.forEach((element, key)=>{
+    element.addEventListener("click", (event)=>{
+      const currentmenu = dropdownSubMenu[key];
+      event.preventDefault();
+      currentmenu.classList.contains("subopen")
+        ?currentmenu.classList.remove("subopen")
+        :currentmenu.classList.add("subopen");
+    });
+  });
+
+  dropdownHeaders.forEach((element, key)=>{
+    dropdownTrigger(
+      dropdownHeaders[key].getAttribute("subtrigger"), 
+      dropdowns[key], 
+      dropdownHeaders[key], 
+      key
+    );
+  });
+
+  // click away handler
+  // hover:
+  document.querySelectorAll("ax-elements .dropdown li[subtrigger='hover']").forEach((element, key) => {
+    document.querySelectorAll("ax-elements .dropdown li[subtrigger='hover'] li").forEach((subElement) => {
+      subElement.addEventListener("mouseleave", ()=>{
+        if(subElement.classList.contains("subopen")) {
+          subElement.classList.remove("subopen");
+        }
+      });
+    });
+  });
+  // click:
+  window.addEventListener("click", (event)=>{
+
+    const dropdownHeaders = document.querySelectorAll("ax-elements .dropdown .dropdownHead");
+    // if the click is on a dropdown
+    let triggerOnDropdown = false;
+    if(event.target.getAttribute("childmode") == "dropdown" ) triggerOnDropdown = true;
+
+    if(!triggerOnDropdown) {
+      dropdownHeaders.forEach((dom, key)=>{
+        const currentmenu = document.querySelectorAll(`ax-elements .dropdown .dropdown[childmodeid='${key}'] .dropdownBody .menu li[subtrigger='click'].subopen`);
+        currentmenu.forEach((element, key)=>{element.classList.remove("subopen")});
+        closeDom(
+          document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key}']`), 
+          document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody`), 
+          document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownHead`), 
+          document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody .menu`),
+        );
+      });
+    }
+    else{
+      dropdownHeaders.forEach((dom, key)=>{
+        if (event.target.getAttribute("childmodeid") != dom.getAttribute("childmodeid")) {
+          const currentmenu = document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody .menu li[subtrigger='click'].subopen`);
+          currentmenu.forEach((element, key)=>{
+            if(!isDescendant(element, event.target))
+              element.classList.remove("subopen")
+          });
+          closeDom(
+            document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key}']`), 
+            document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody`), 
+            document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownHead`), 
+            document.querySelector(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody .menu`),
+          )
+        }
+      });
+    }
+  });
+
+
+  function isDescendant(parent, child) {
+    var node = child.parentNode;
+    while (node != null) {
+        if (node == parent) {
+            return true;
+        }
+        node = node.parentNode;
+    }
+    return false;
+  }
+
+  function dropdownTrigger(trigger, dropdown, dropdownHeader, key) {
+    switch(trigger) {
+      case 'click': dropdownClickTrigger(dropdownHeader, key); break;
+      case 'hover': dropdownHoverTrigger(dropdown, key); break;
+      default: break;
+    }
+  }
+
+  function dropdownHoverTrigger(element, key) {
+    element.addEventListener("mouseenter", ()=>{dropdownHandler(key)});
+    element.addEventListener("mouseleave", ()=>{
+      const currentmenu = document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key+1}'] .dropdownBody .menu li[subtrigger='click'].subopen`);
+      currentmenu.forEach((element1, key)=>{
+        element1.classList.remove("subopen");
+      });
+      dropdownHandler(key)
+    });
+  }
+
+  function dropdownClickTrigger(element, key) {
+    element.addEventListener("click", ()=>{dropdownHandler(key)});
+  }
+
+  document.querySelectorAll(".dropdown.mega .dropdownBody .menu").forEach(element=>{
+    element.addEventListener("scroll", (event)=>{
+      element.querySelector(".dropdownHeadTitle").style.opacity = element.scrollTop === 0 ? 1 : 1-((element.scrollTop)/56);
+    });
+  });
+
+
+  // head title settings
+  document.querySelectorAll(`ax-elements:not([nomain="true"]) > section.dropdown`).forEach((element)=>{
+    if(document.querySelector(`.dropdown .dropdownBody[childmodeid='${element.getAttribute("childmodeid")}'] .menu .dropdownHeadTitle`)) {
+      const inner = element.querySelector(".dropdownHead .inner");
+      document.querySelector(`.dropdown .dropdownBody[childmodeid='${element.getAttribute("childmodeid")}'] .menu .dropdownHeadTitle`).style.color = inner.getAttribute("headTitleColor");
+    }
+  });
+
+
+  // style on hover
+  document.querySelectorAll(`ax-elements:not([nomain="true"]) > section.dropdown`).forEach((element)=>{
+    const inner = element.querySelector(".dropdownHead .inner");
+    element.addEventListener("mouseover", ()=>{
+      if(!element.classList.contains("open")) {
+        element.querySelector(".dropdownHead .inner .dropicon").src = "https://axoncodes.com/libraries/dropdown/assets/icons/down-white.svg";
+        inner.style.backgroundColor = inner.getAttribute("headbackgroundhover");
+        inner.style.color = inner.getAttribute("colorhover");
+        
+      }
+    });
+
+    element.addEventListener("mouseout", ()=>{
+      if(!element.classList.contains("open")) {
+        element.querySelector(".dropdownHead .inner .dropicon").src = "https://axoncodes.com/libraries/dropdown/assets/icons/down.svg";
+        inner.style.backgroundColor = inner.getAttribute("headbackground");
+        inner.style.color = inner.getAttribute("color");
+      }
+    });
+  });
+
+
+  function dropdownHandler(key) {
+
+    // declare
+    const dropdown = document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key}']`);
+    const head = document.querySelector(`ax-elements .dropdown .dropdownHead[childmodeid='${key}']`);
+    const body = document.querySelector(`ax-elements .dropdown .dropdownBody[childmodeid='${key}']`);
+    const menu = document.querySelector(`ax-elements .dropdown .dropdownBody .menu[childmodeid='${key}']`);
+    const lists = document.querySelectorAll(`ax-elements .dropdown[childmodeid='${key}'] .dropdownBody .menu li`);
+    
+    // open/close handler
+    dropdown[0].classList.contains("open") 
+      ?closeDom(dropdown, body, head, menu)
+      :openDom(dropdown, body, head, lists, menu);
+      
+    // subopening handler
+    document.querySelectorAll(`ax-elements .dropdown[childmodeid="${key+1}"] .dropdownBody .menu li.side > ul`).forEach((dom, key2)=>{
+      // handleTheDropdownsFallingOutOfView(key+1, dom);
+    });
+
+    // custom --mega
+    if(body.getAttribute('mode') == "mega") {
+      body.style.left = "-"+dropdown.offsetLeft+"px";
+    }
+
+
+  }
+
+  function handleTheDropdownsFallingOutOfView(key, actualdom) {
+    let righthand = document.documentElement.offsetWidth-actualdom.getBoundingClientRect().x-actualdom.getBoundingClientRect().width;
+    if(righthand<0) {
+      actualdom.style.left = "unset";
+      actualdom.style.right = "-"+(actualdom.getBoundingClientRect().width+actualdom.getBoundingClientRect().right)+"px";
+    } else {
+      actualdom.style.right = "unset";
+      actualdom.style.left = actualdom.getBoundingClientRect().width+"px";
+    }
+  }
+
+  function closeDom(dropdown, body, head, menu) {
+    if(dropdown.length>1) {
+      dropdown[0].classList.remove("open");
+      dropdown[1].classList.remove("open");
+    }else dropdown[0].classList.remove("open");
+    body.classList.remove("open");
+    head.classList.remove("open");
+    head.querySelector(".inner").style.backgroundColor=head.querySelector(".inner").getAttribute("headbackground");
+    head.querySelector(".inner").style.color=head.querySelector(".inner").getAttribute("color");
+    head.querySelector(".inner .dropicon").src="https://axoncodes.com/libraries/dropdown/assets/icons/down.svg";
+    menu.style.maxHeight = 0, 
+    body.style.maxHeight = 0
+  }
+
+  function openDom(dropdown, body, head, lists, menu) {
+    var height = 0, minMenuHeight = menu.clientHeight;
+    if(dropdown.length>1) {
+      dropdown[0].classList.add("open");
+      dropdown[1].classList.add("open");
+    } else dropdown[0].classList.add("open");
+    body.classList.add("open");
+    head.classList.add("open");
+    head.querySelector(".inner").style.backgroundColor=head.querySelector(".inner").getAttribute("activebackground");
+    head.querySelector(".inner").style.color=head.querySelector(".inner").getAttribute("colorhover");
+    head.querySelector(".inner .dropicon").src="https://axoncodes.com/libraries/dropdown/assets/icons/down-white.svg";
+    lists.forEach((list)=>{
+      height += list.clientHeight;
+      menu.style.maxHeight = (height+minMenuHeight)+"px";
+      body.style.maxHeight = (height+minMenuHeight)+"px";
+    });
+  
+    if(dropdown[0].getAttribute("mode").indexOf("mega") >= 0) {
+      const thereductionrate = dropdown[0].getAttribute("targetlocator")?148:18;
+      const megaheight = (window.innerHeight - body.offsetTop)-thereductionrate;
+      body.style.height = megaheight+"px";
+      body.querySelector(".menu").style.height = (megaheight)+"px";
+      body.querySelector(".menu").style.width = (document.documentElement.offsetWidth-0.18*document.documentElement.offsetWidth)+"px";
+    }
+    if(dropdown[0].getAttribute("mode").indexOf("simple") >= 0) {
+      if(document.querySelector(`section.dropdown ul.menu[childmodeid="${body.getAttribute("childmodeid")}"]`)) {
+        const mainDropdownLeft = document.querySelector(`section.dropdown.mainDropdown[childmodeid="${body.getAttribute("childmodeid")}"]`).getBoundingClientRect().x;
+        document.querySelector(`section.dropdown ul.menu[childmodeid="${body.getAttribute("childmodeid")}"]`).style.left = `${mainDropdownLeft}px`;
+      }
+      // handleTheDropdownsFallingOutOfView(body.getAttribute("childmodeid"), menu);
+
+    }
+    
+  }
+
+
+  // active megadropdown size handler
+  function megasizehandler() {
+    console.log(document.documentElement.offsetWidth, window.innerWidth);
+    document.querySelectorAll('.dropdown.open').forEach(element => {
+      if(element.getAttribute("mode") && element.getAttribute("mode").indexOf("mega") >= 0) {
+        var body;
+        if(element.getAttribute("targetlocator")) body = document.querySelector(`#${element.getAttribute("targetlocator")} .dropdownBody`);
+        else body = element.querySelector(".dropdownBody");
+        const thereductionrate = element.getAttribute("targetlocator")?148:9;
+        const megaheight = (window.innerHeight - (body.offsetTop + element.offsetTop))-thereductionrate;
+        body.style.height = megaheight+"px";
+        body.querySelector(".menu").style.height = (megaheight)+"px";
+        body.querySelector(".menu").style.width = (document.documentElement.offsetWidth-0.18*document.documentElement.offsetWidth)+"px";
+      }
+    });
+
+
+    document.querySelectorAll('.dropdown').forEach(element => {
+      if(element.getAttribute("mode") && element.getAttribute("mode").indexOf("simple") >= 0) {
+        // the 'left' for simple dropdowns with 'nomain'
+        if(document.querySelector(`section.dropdown ul.menu[childmodeid="${element.getAttribute("childmodeid")}"]`)) {
+          const mainDropdownLeft = document.querySelector(`section.dropdown.mainDropdown[childmodeid="${element.getAttribute("childmodeid")}"]`).getBoundingClientRect().x;
+          document.querySelector(`section.dropdown ul.menu[childmodeid="${element.getAttribute("childmodeid")}"]`).style.left = `${mainDropdownLeft}px`;
+        }
+      }
+    });
+
+  } window.addEventListener("resize", megasizehandler);
+  megasizehandler();
+
+
+});
---
+++ registery-sample.js
@@ -0,0 +1,154 @@
+// counters
+var dropdownsCount = 0;
+
+// module handlers
+customElements.define('ax-elements', class AxElements extends HTMLElement {
+    connectedCallback() {
+        switch(this.getAttribute('mode')) {
+            case "dropdown": axCustomDropdown(this); break;
+            case "dropdownGroup": axCustomDropdownGroup(this); break;
+            case "logo": axCustomLogo(this); break;
+            default: break;
+        }
+    }
+});
+
+
+// dropdown group --start
+function axCustomDropdownGroup(element) {
+    var attrs = {
+        mode: element.attributes.mode?element.attributes.mode.value:null,
+        title: element.attributes.title?element.attributes.title.value:null,
+        color: element.attributes.color?element.attributes.color.value:null,
+        colorHover: element.attributes.colorHover?element.attributes.colorHover.value:null,
+        activeBackground: element.attributes.activeBackground?element.attributes.activeBackground.value:null,
+        headBackground: element.attributes.headBackground?element.attributes.headBackground.value:null,
+        headBackgroundHover: element.attributes.headBackgroundHover?element.attributes.headBackgroundHover.value:null,
+    };
+    
+}
+// dropdown group --finish
+
+
+
+
+// dropdown --start
+function axCustomDropdown(element) {
+    if(!element.getAttribute('nomain')){
+        var attrs = {
+            subTrigger: element.attributes.subTrigger?element.attributes.subTrigger.value:'click',
+            targetLocator: element.attributes.targetLocator?element.attributes.targetLocator.value:null,
+            color: element.attributes.color?element.attributes.color.value:null,
+            exit: element.attributes.exit?element.attributes.exit.value:null,
+            headTitle: element.attributes.headTitle?element.attributes.headTitle.value:null,
+            headTitleColor: element.attributes.headTitleColor?element.attributes.headTitleColor.value:null,
+            colorHover: element.attributes.colorHover?element.attributes.colorHover.value:null,
+            activeBackground: element.attributes.activeBackground?element.attributes.activeBackground.value:null,
+            headBackground: element.attributes.headBackground?element.attributes.headBackground.value:null,
+            headBackgroundHover: element.attributes.headBackgroundHover?element.attributes.headBackgroundHover.value:null,
+            title: element.attributes.title?element.attributes.title.value:null,
+            icon: element.attributes.icon?element.attributes.icon.value:null,
+            level: element.attributes.level?element.attributes.level.value:null,
+            mode: element.attributes.mode?element.attributes.mode.value:null,
+            background: element.attributes.background?element.attributes.background.value:null,
+            structure: element.attributes.structure?element.attributes.structure.value:"simple",
+            options: element.attributes.options?JSON.parse(element.attributes.options.value):null,
+            width: parseInt(element.attributes.width?element.attributes.width.value:null),
+            height: parseInt(element.attributes.height?element.attributes.height.value:'22'),
+        };
+        const childMode = `childMode="${attrs.mode}" childModeId="${dropdownsCount}" `;
+        const inner = element.innerHTML; 
+
+        const dropdownHead = `
+            <div ${childMode} style="height:${attrs.height+"px"}; ${attrs.width?`width:${((attrs.width)+4)+"px"}`:``}" class="dropdownHead" subtrigger="${attrs.subTrigger}" mode="${attrs.structure}">
+                <div ${childMode} 
+                activeBackground="${attrs.activeBackground}" 
+                colorHover="${attrs.colorHover}" 
+                headBackgroundHover="${attrs.headBackgroundHover}" 
+                color="${attrs.color}"
+                headTitleColor="${attrs.headTitleColor}"
+                headBackground="${attrs.headBackground}" 
+                class="inner"
+                style="${attrs.width?`min-width:${((attrs.width)-10)+"px"}; width:${((attrs.width)-10)+"px"}`:``}">
+                    ${attrs.icon?`<img ${childMode} class="icon" src="./dropdown/assets/icons/${attrs.icon}">`:``}
+                    <span ${childMode}>${attrs.title}</span> 
+                    <img ${childMode} class="dropicon" src="https://axoncodes.com/libraries//dropdown/assets/icons/down.svg">
+                </div>
+            </div>`;
+            
+        const { content, count } = dropdownContent_handler(attrs.options, childMode);
+
+        const dropdownList = content;
+        const dropdownBody = `
+            <div ${childMode} class="dropdownBody" mode="${attrs.structure}" style="${attrs.width?`min-width:${attrs.width+"px"}`:``}">
+                <ul ${childMode} style="background-color:${attrs.background};" class="menu">
+                    ${attrs.headTitle?`<h3 class="dropdown dropdownHeadTitle" ${childMode}>${attrs.headTitle}</h3>`:``}
+                    ${attrs.exit?`<svg class="exitbutton" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>`:``}
+                    ${dropdownList}
+                    ${inner}
+                </ul>
+            </div>`;
+        if(attrs.targetLocator && attrs.targetLocator.length>0) {
+            document.getElementById(attrs.targetLocator).classList.add(`dropdown-${dropdownsCount}`);
+            document.getElementById(attrs.targetLocator).innerHTML = `<section ${childMode} class="dropdown">${dropdownBody}</section>`;
+            element.innerHTML = `<section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" targetLocator="${attrs.targetLocator}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead}</section>`;
+            dropdownsCount++;
+        }else{
+            dropdownsCount++;
+            element.innerHTML = `<section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead} ${dropdownBody}</section>`;
+        }
+    }
+}
+
+
+// dropdown content handler
+/**
+ * @item.subOpening : sub, side
+ * @item.subTrigger : click, hover
+ */
+function dropdownContent_handler(data, childMode) {
+    var content = ``;
+    let count=0;
+    if(data) {
+        data.map((item)=>{
+            count++;
+            content += `
+            <li ${childMode} subTrigger="${item.subTrigger?item.subTrigger:'click'}" class="list ${item.subOpening?item.subOpening:'sub'} ${item.level?item.level:''}">
+                <div ${childMode} class="listHead">
+                    ${item.url?`<a ${childMode} href="${item.url}" class="inner">`:`<div class="inner">`}
+                        ${item.icon?`<img ${childMode} src="./dropdown/assets/icons/${item.icon.url}"/>`:`<span></span>`}
+                        ${item.title?`<span style="color:${item.color}" ${childMode}>${item.title}</span>`:``}
+                        ${item.content?`<img ${childMode} class="icon" src="https://axoncodes.com/libraries//dropdown/assets/icons/down.svg">`:`<span></span>`}
+                    ${item.url?`</a>`:`</div>`}
+                </div>
+                <ul ${childMode} class="listSubmenu">
+                    ${item.content?dropdownContent_handler(item.content, childMode).content:``}
+                </ul>
+            </li>`;
+        });
+    }
+    return {
+        content,
+        count
+    };
+
+}
+// dropdown --finish
+
+
+
+// logo --start
+function axCustomLogo(element) {
+    var attrs = {
+        alt: element.attributes.alt?element.attributes.alt.value:null,
+        link: element.attributes.link?element.attributes.link.value:null,
+        src: element.attributes.src?element.attributes.src.value:null,
+        mode: element.attributes.mode?element.attributes.mode.value:null,
+    };
+    element.innerHTML = `
+        <a href="${attrs.link}" class="headerLogo">
+            <img src="${attrs.src}" alt="${attrs.alt}" />
+        </a>
+    `;
+}
+// logo --finish// counters
