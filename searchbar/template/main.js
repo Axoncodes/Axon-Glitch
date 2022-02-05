61599feea0fbd4ff470d4974c1fcf32a18e90b30
Axoncodes <devops@axoncodes.com>
2022-02-05 17:09:19 -0500

    Implementing hideAtTop for searchbar

--- searchbar/template/main.js
+++ searchbar/template/main.js
@@ -4,4 +4,14 @@ if (document.getElementById("axg_searchform")) {
     document.getElementById("axg_searchform_res_cover").style.display="block";
     activationHandler.start('axg_searchbar')
   });
+
+  // hideAtTop
+  const searchbar = document.getElementById('axg_searchbar')
+  const hideAtTop = parseInt(searchbar.getAttribute('hideAtTop'))
+  window.addEventListener("scroll", () => {
+    if (hideAtTop) {
+      searchbar.classList.add('axg_hide')
+      if (window.scrollY > 200) searchbar.classList.remove('axg_hide')
+    }
+  });
 }
