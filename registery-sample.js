03c404965a5042584ef33381d66ec5813d2562a5
Axoncodes <devops@axoncodes.com>
2022-02-11 11:21:23 -0500

    justLink feature for dropdown & custom placeholder for searchbar & cleanup

--- registery-sample.js
+++ registery-sample.js
@@ -19,15 +19,16 @@ customElements.define('ax-elements', class AxElements extends HTMLElement {
 // axCustomsearchbar --start
 function axCustomsearchbar(element) {
 	const regExp = /[^'\x22]+/;
+    const ele = element.attributes
 	element.outerHTML = `
-    <div id="axg_searchbar" hideAtTop="${element.attributes.hideAtTop?element.attributes.hideAtTop.value:null}">
+    <div id="axg_searchbar" hideAtTop="${ele.hideAtTop?ele.hideAtTop.value:null}">
         <form id="axg_searchform" method="POST">
         <label for="axg_searchform_button">search</label>
         <button type="submit" name="ax_submit" title="ax_submit" id="axg_searchform_button">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"> <g> <g> <path d="M447.615,64.386C406.095,22.866,350.892,0,292.175,0s-113.92,22.866-155.439,64.386 C95.217,105.905,72.35,161.108,72.35,219.824c0,53.683,19.124,104.421,54.132,144.458L4.399,486.366 c-5.864,5.864-5.864,15.371,0,21.236C7.331,510.533,11.174,512,15.016,512s7.686-1.466,10.617-4.399l122.084-122.083 c40.037,35.007,90.775,54.132,144.458,54.132c58.718,0,113.919-22.866,155.439-64.386c41.519-41.519,64.385-96.722,64.385-155.439 S489.134,105.905,447.615,64.386z M426.379,354.029c-74.001,74-194.406,74-268.407,0c-74-74-74-194.407,0-268.407 c37.004-37.004,85.596-55.5,134.204-55.5c48.596,0,97.208,18.505,134.204,55.5C500.378,159.621,500.378,280.028,426.379,354.029z" /></g></g></svg>
         </button>
         <label for="axg_isearch">search</label>
-        <input id="axg_isearch" placeholder="search..." type="search" name="axg_isearch" pattern="${regExp}"/>
+        <input customplaceholder="${ele.placeholder?ele.placeholder.value:null}" style="${ele.inputColor?`background-color: ${ele.inputColor.value};`:null}" id="axg_isearch" placeholder="search..." type="search" name="axg_isearch" pattern="${regExp}"/>
         </form>
 
         <div id="axg_searchform_res_cover">
@@ -74,28 +75,35 @@ function axCustomDropdownGroup(element) {
 
 // dropdown --start
 function axCustomDropdown(element) {
+    const ele = element.attributes;
     if(!element.getAttribute('nomain')){
         var attrs = {
-            subTrigger: element.attributes.subTrigger?element.attributes.subTrigger.value:'click',
-            targetLocator: element.attributes.targetLocator?element.attributes.targetLocator.value:null,
-            color: element.attributes.color?element.attributes.color.value:null,
-            exit: element.attributes.exit?element.attributes.exit.value:null,
-            headTitle: element.attributes.headTitle?element.attributes.headTitle.value:null,
-            headTitleColor: element.attributes.headTitleColor?element.attributes.headTitleColor.value:null,
-            colorHover: element.attributes.colorHover?element.attributes.colorHover.value:null,
-            activeBackground: element.attributes.activeBackground?element.attributes.activeBackground.value:null,
-            headBackground: element.attributes.headBackground?element.attributes.headBackground.value:null,
-            headBackgroundHover: element.attributes.headBackgroundHover?element.attributes.headBackgroundHover.value:null,
-            title: element.attributes.title?element.attributes.title.value:null,
-            icon: element.attributes.icon?element.attributes.icon.value:null,
-            level: element.attributes.level?element.attributes.level.value:null,
-            mode: element.attributes.mode?element.attributes.mode.value:null,
-            background: element.attributes.background?element.attributes.background.value:null,
-            structure: element.attributes.structure?element.attributes.structure.value:"simple",
-            options: element.attributes.options?JSON.parse(element.attributes.options.value):null,
-            width: parseInt(element.attributes.width?element.attributes.width.value:null),
-            height: parseInt(element.attributes.height?element.attributes.height.value:'22'),
+            subTrigger: ele.subTrigger?ele.subTrigger.value:'click',
+            targetLocator: ele.targetLocator?ele.targetLocator.value:null,
+            color: ele.color?ele.color.value:null,
+            exit: ele.exit?ele.exit.value:null,
+            headTitle: ele.headTitle?ele.headTitle.value:null,
+            headTitleColor: ele.headTitleColor?ele.headTitleColor.value:null,
+            colorHover: ele.colorHover?ele.colorHover.value:null,
+            activeBackground: ele.activeBackground?ele.activeBackground.value:null,
+            headBackground: ele.headBackground?ele.headBackground.value:null,
+            headBackgroundHover: ele.headBackgroundHover?ele.headBackgroundHover.value:null,
+            title: ele.title?ele.title.value:null,
+            icon: ele.icon?ele.icon.value:null,
+            level: ele.level?ele.level.value:null,
+            mode: ele.mode?ele.mode.value:null,
+            background: ele.background?ele.background.value:null,
+            structure: ele.structure?ele.structure.value:"simple",
+            options: ele.options?JSON.parse(ele.options.value):null,
+            width: parseInt(ele.width?ele.width.value:null),
+            height: parseInt(ele.height?ele.height.value:'22'),
+            link: ele.link?ele.link.value:null,
+            breakpoint: parseInt(ele.breakpoint?ele.breakpoint.value:'700'),
         };
+        // update css variable for breakpoint
+        let root = document.documentElement;
+        root.style.setProperty('--dropdownGroup-breakpoint', attrs.breakpoint + "px");
+
         const childMode = `childMode="${attrs.mode}" childModeId="${dropdownsCount}" `;
         const inner = element.innerHTML; 
 
@@ -115,30 +123,32 @@ function axCustomDropdown(element) {
                     grid-template-columns:${attrs.icon?`max-content auto max-content`:`max-content auto`};
                 ">
                     ${attrs.icon?`<img ${childMode} class="icon" src="./dropdown/assets/icons/${attrs.icon}">`:``}
-                    <span ${childMode}>${attrs.title}</span> 
-                    <img ${childMode} class="dropicon" src="https://axoncodes.com/libraries//dropdown/assets/icons/down.svg">
+                    <span ${childMode}>${attrs.link ? `<a style="color:${attrs.color}" href="${attrs.link}">`:''}${attrs.title}${attrs.link ? `</a>`:''}</span>
+                    ${attrs.structure != 'link' ? `<img ${childMode} class="dropicon" src="https://api.axoncodes.com/libraries/dropdown/assets/icons/down.svg">` : ''}
                 </div>
             </div>`;
-            
+
         const dropdownList = dropdownContent_handler(attrs, attrs.options, childMode);
         const dropdownBody = `
             <div ${childMode} class="dropdownBody" mode="${attrs.structure}" style="${attrs.width?`min-width:${attrs.width+"px"}`:``}">
                 <ul ${childMode} style="background-color:${attrs.background};" class="menu">
                     ${attrs.headTitle?`<h3 class="dropdown dropdownHeadTitle" ${childMode}>${attrs.headTitle}</h3>`:``}
                     ${attrs.exit?`<svg class="exitbutton" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>`:``}
-                    ${dropdownList}
+                    ${attrs.structure != 'link' ? dropdownList : ''}
                     ${inner}
                 </ul>
             </div>`;
-        if(attrs.targetLocator && attrs.targetLocator.length>0) {
-            document.getElementById(attrs.targetLocator).classList.add(`dropdown-${dropdownsCount}`);
-            document.getElementById(attrs.targetLocator).innerHTML = `<section ${childMode} class="dropdown">${dropdownBody}</section>`;
-            element.outerHTML = `<section class="ax_elements"><section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" targetLocator="${attrs.targetLocator}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead}</section></section>`;
-            dropdownsCount++;
-        }else{
-            dropdownsCount++;
-            element.outerHTML = `<section class="ax_elements"><section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead} ${dropdownBody}</section></section>`;
-        }
+        if (attrs.structure != 'link') {
+            if(attrs.targetLocator && attrs.targetLocator.length>0) {
+                document.getElementById(attrs.targetLocator).classList.add(`dropdown-${dropdownsCount}`);
+                document.getElementById(attrs.targetLocator).innerHTML = `<section ${childMode} class="dropdown">${dropdownBody}</section>`;
+                element.outerHTML = `<section class="ax_elements"><section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" targetLocator="${attrs.targetLocator}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead}</section></section>`;
+                dropdownsCount++;
+            }else{
+                dropdownsCount++;
+                element.outerHTML = `<section class="ax_elements"><section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead} ${dropdownBody}</section></section>`;
+            }
+        } else element.outerHTML = `<section class="ax_elements"><section style="${attrs.width?`min-width:${attrs.width+"px"}`:``}" mode="${attrs.structure}" ${childMode} class="dropdown mainDropdown ${attrs.structure}">${dropdownHead}</section></section>`;
     }
 }
 
@@ -158,7 +168,7 @@ function dropdownContent_handler(attrs, data, childMode) {
                     ${item.url?`<a ${childMode} href="${item.url}" class="inner">`:`<div class="inner">`}
                         ${item.icon?`<img ${childMode} src="./dropdown/assets/icons/${item.icon.url}"/>`:`<span></span>`}
                         ${item.title?`<span style="color:${item.color}" ${childMode}>${item.title}</span>`:``}
-                        ${item.content?`<img ${childMode} class="icon" src="https://axoncodes.com/libraries//dropdown/assets/icons/down.svg">`:`<span></span>`}
+                        ${item.content?`<img ${childMode} class="icon" src="https://api.axoncodes.com/libraries//dropdown/assets/icons/down.svg">`:`<span></span>`}
                     ${item.url?`</a>`:`</div>`}
                 </div>
                 <ul ${childMode} class="listSubmenu">
